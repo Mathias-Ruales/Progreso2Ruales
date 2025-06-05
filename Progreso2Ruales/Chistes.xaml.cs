f74187f8a49965e2e9eba0d2cf75160efcd6b424
Mathias-Ruales <mathias.david.ruales@gmail.com>
2025-06-05 08:43:44 -0500

    Chistes

--- Progreso2Ruales/Chistes.xaml.cs
+++ Progreso2Ruales/Chistes.xaml.cs
@@ -1,9 +1,26 @@
+using Progreso2Ruales.Services;
+
 namespace Progreso2Ruales;
 
 public partial class Chistes : ContentPage
 {
-	public Chistes()
-	{
-		InitializeComponent();
-	}
+    private readonly JokesApi _jokeService = new JokesApi();
+
+    public Chistes()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnGetJokeClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            var joke = await _jokeService.GetRandomJokeAsync();
+            ChisteText.Text = $"{joke.Setup}\n\n{joke.Punchline}";
+        }
+        catch (Exception)
+        {
+            ChisteText.Text = "Ocurrió un error al obtener el chiste.";
+        }
+    }
 }
---
+++ Progreso2Ruales/Interfaz/JokesApi.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Progreso2Ruales.Services
+{
+    public class JokesApi
+    {
+        private readonly HttpClient _httpClient;
+
+        public JokesApi()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<Chiste> GetRandomJokeAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<Chiste>("https://official-joke-api.appspot.com/random_joke");
+        }
+    }
+
+    public class Chiste
+    {
+        public string Type { get; set; }
+        public string Setup { get; set; }
+        public string Punchline { get; set; }
+    }
+}
